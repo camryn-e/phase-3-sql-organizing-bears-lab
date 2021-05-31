888d3cac95b4dbea8c4f96aa32f2afdf6c870580
camryn williams <williams.camryn.e@gmail.com>
2021-05-31 15:06:18 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,14 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "M", "brown", "chill", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 1, "F", "black", "playful", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 10, "F", "b&w", "she protecc", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 30, "M", "brown", "sleepy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 3, "F", "black", "friendly", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 10, "M", "brown", "angry boi", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 11, "F", "b&w", "aloof", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 16, "M", "brown", "aggressive", 0);
+
+
+
+
+
+
